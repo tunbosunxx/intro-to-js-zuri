dbb083b676b1d8c431ada1b48f3b9fff5a009761
Olatunbosun Agunbiade <borsmanty22@gmail.com>
2022-06-21 20:06:35 +0200

    initial commit

---
+++ zurijavaintro.js
@@ -0,0 +1,9 @@
+var str;
+str ='Olatunbosun Agunbiade'
+console.log('NAME: ' + str)
+var num;
+num = 6.2
+console.log('HEIGHT: ' + num + 'ft')
+var msg;
+msg = 'Nigeria'
+console.log('COUNTRY: ' + msg)
